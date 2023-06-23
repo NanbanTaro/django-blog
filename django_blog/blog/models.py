235654310858e50b6eafcb0afd45a4969739bbdb
NanbanTaro <>
2023-06-23 20:55:25 +0900

    created blog database

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=40)
+    text = models.TextField()
+    posted_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.text
