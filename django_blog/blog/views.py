c73d0c247635e4d4a71edfe59293e63df2d50b7a
NanbanTaro <>
2023-06-23 19:12:40 +0900

    created base page and detail page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    params = {
+        'title': 'テストページ',
+        'message': 'テストページです。'
+    }
+    return render(request, 'blog/index.html', params)
+
+def detail(request):
+    params = {
+        'title': '詳細ページ',
+        'message': '詳細ページです。'
+    }
+    return render(request, 'blog/detail.html', params)
